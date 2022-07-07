0bfa0c584cbf19f830143de7aa60b22e901daa3f
Achra-f <achraf.bendella@student.odisee.be>
2022-07-07 22:20:08 +0200

    Finish javascript fundamentals part 1

---
+++ js-basics/javascript.js
@@ -0,0 +1,31 @@
+
+
+console.log(99 + 1);
+
+
+console.log((4 + 6 + 9) / 77);
+
+
+
+let a = 10;
+
+console.log(a)
+
+console.log(a * 9)
+
+
+let b = 7 * a;
+
+console.log(b);
+
+
+const max = 57;
+
+let actual = max - 13;
+
+let percentage = actual / max;
+
+
+
+
+
